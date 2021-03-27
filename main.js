f5ded91a5bc7b8342001791b342e30cfc6b75a8a
theiconik <kumarujjawal51@gmail.com>
2021-03-28 00:52:18 +0530

    Populating DOM with the weather API object fetched

--- main.js
+++ main.js
@@ -22,4 +22,30 @@ function getResults (query) {
 
 function displayResults (weather) {
    console.log(weather);
+   let city = document.querySelector(".location .city");
+   city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+   let now = new Date();
+   let date = document.querySelector(".location .date");
+   date.innerText = dateBuilder(now);
+
+   let temp = document.querySelector(".current .temp");
+   temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`
+
+   let weather_el = document.querySelector(".current .weather");
+   weather_el.innerText = weather.weather[0].main;
+
+   let hilow= document.querySelector(".hi-low");
+   hilow.innerText = `${weather.main.temp_min}°C / ${weather.main.temp_max}°C`
+}
+
+function dateBuilder (d) {
+   let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+   let days= ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+   let day = days[d.getDay()];
+   let date= d.getDate();
+   let month = months[d.getMonth()];
+   let year= d.getFullYear(); 
+
+   return `${day}, ${date} ${month} ${year}`;
 }
